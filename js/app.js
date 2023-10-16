ad3d0b6f144bd4522c4f09a970f8e85b0339859d
couchjanus <couchjanus@gmail.com>
2023-10-16 20:55:48 +0300

    added unit 2

--- js/app.js
+++ js/app.js
@@ -1,84 +1,86 @@
 'use strict';
-// js script
-// window.alert("hello from js file")
 
-/**
- * documentations
- */
 
-var res = 2 + 2; 
-// alert(res);
+// let obj = {
+//     key: "Hello",
+//     numb: 123,
+//     'my key': "Hello world",
+//     test1: function*() {
+//         return "This is test";
+//     }
+// }
+// console.log(obj.key);
+// console.log(obj.numb);
+// console.log(obj['my key']);
+// console.log(typeof(obj))
+// let tst = obj.test1();
+// console.log(tst);
 
-let a = 55;
-let b = 77;
-const test234_test_test$ = 'hello'
-let _test = ""
-let $_test = ""
-let A = 55;
-// for = 77
-const city = "Kyiv"
-let RES = null;
-console.log(typeof(RES));
-console.log(typeof(res));
-{
-    let a = "hello"
-    let b = "world"
+// console.dir(window.document.body);
+// console.dir(window.document.head);
+// console.dir(window.document.title);
 
-    // alert (a + b);
+// console.dir(document.all);
+// console.dir(document.all[55]);
 
-    const city = "LA";
+// document.all[55].innerText = "Hello DOM!";
+// let h2_collection = document.querySelectorAll('h2');
+// console.log(h2_collection[2]);
 
-    // alert(res);
-    res = "It's string";
-    // alert(res);
+// h2_collection[2].innerText = "Hello item number 2";
+
+// let product_collection = document.querySelectorAll('.product');
+// let product_collection = document.getElementsByClassName('product');
+// console.log(product_collection[2]);
+
+// let h2_first = document.querySelector('h2');
+// console.log(h2_first);
+
+
+// toggler
+// console.log(toggler);
+
+
+// let this_toggler = document.getElementById('toggler');
+
+// console.log(this_toggler);
+
+function main() {
+    const totalInWishlist  = document.getElementById('total-in-wishlist');
+    const totalInCart  = document.getElementById('total-in-cart');
+    
+    let addToWishlist = document.querySelector('.add-to-wishlist');
+    let addToCart = document.querySelector('.add-to-cart');
+    
+    const changeStyle = (item) => item.classList.add(["not-empty"]);
+    // console.log(totalInWishlist.innerText);
+    // console.log(totalInWishlist.innerText + 1 == true);
+    addToWishlist.addEventListener('click', function() {
+        // totalInWishlist.style.color = "red";
+        // totalInWishlist.style.cssText = "color:red; opacity:1; font-weight:700";
+        // totalInWishlist.className = 'not-empty';
+    
+        if (!+totalInWishlist.innerText) {
+            console.log(totalInWishlist.innerText);
+            changeStyle(totalInWishlist);
+        }
+        // changeStyle(totalInWishlist);
+        totalInWishlist.innerText = ++totalInWishlist.innerText;
+    });
+    
+    addToCart.addEventListener('click', function() {
+    
+        if (!+totalInCart.innerText) {
+           
+            changeStyle(totalInCart);
+        }
+    
+        totalInCart.innerText = ++totalInCart.innerText;
+    });
 }
-// city = "LA";
-// alert (a + b);
 
-function add(x, y) {
-    return x + y;
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", () => main())
+} else {
+    main();
 }
-
-function hello() {
-    console.log("Hello World");
-}
-
-
-hello();
-console.log(add(4.8, 4.7));
-console.log(Math.sin(4.8, 4.7));
-
-console.log(typeof(res));
-
-console.log(typeof(hello));
-
-console.log(3 != 1);
-
-let x, y, z, o;
-x = 7;
-y = 9;
-o = '*';
-if (o == '+') {
-    z = x + y;
-}
-else if (o == '-') {
-    z = x - y;
-}
-else if (o == '*') {
-    z = x * y;
-}
-else {
-    console.log("Unknow operation");
-}
-
-console.log("Result = ", z);
-
-
-switch (prompt("Enter operator:")) {
-    case "+":   alert("x + y ="+(x+y)); break;
-    case "-":    alert("x - y ="+(x-y)); break;
-    case "*":    alert("x * y ="+(x*y));
-    case "/":    alert("x / y ="+(x/y)); break;
-    default:     alert("Непевна операція!"); break;
- }
- 
